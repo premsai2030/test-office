b9cd3fd2082fb4f4290eb69d4dc942ee36754195
premsai2030 <premsai2030@gmail.com>
2023-01-13 22:09:04 +0530

    RESOLVED ERRORS

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App() {
       convert(speech);
       setSpeak(false);
     }
-  }, [speak]);
+  }, [convert, speak, speech]);
 
   function gotoAnnotation(values) {
     setTo(values.camPos);
@@ -186,27 +186,27 @@ function App() {
 
 export default App;
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef();
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-      castShadow={true}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+// function Box(props) {
+//   // This reference gives us direct access to the THREE.Mesh object
+//   const ref = useRef();
+//   // Hold state for hovered and clicked events
+//   const [hovered, hover] = useState(false);
+//   const [clicked, click] = useState(false);
+//   // Subscribe this component to the render-loop, rotate the mesh every frame
+//   useFrame((state, delta) => (ref.current.rotation.x += delta));
+//   // Return the view, these are regular Threejs elements expressed in JSX
+//   return (
+//     <mesh
+//       {...props}
+//       ref={ref}
+//       scale={clicked ? 1.5 : 1}
+//       onClick={(event) => click(!clicked)}
+//       onPointerOver={(event) => hover(true)}
+//       onPointerOut={(event) => hover(false)}
+//       castShadow={true}
+//     >
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+//     </mesh>
+//   );
+// }
